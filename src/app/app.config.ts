aa2071aa3230b7f7bf4858164eec165a2f6655db
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-05-19 12:48:59 +0200

    installing process

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3rJIvAu0GK3BTHBwxxuho6u97cvaE2bc",
+  authDomain: "angular-firebase-auth-ud-78a81.firebaseapp.com",
+  projectId: "angular-firebase-auth-ud-78a81",
+  storageBucket: "angular-firebase-auth-ud-78a81.firebasestorage.app",
+  messagingSenderId: "1049255408577",
+  appId: "1:1049255408577:web:3c64e4b31aa5cf6c65f540"
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
